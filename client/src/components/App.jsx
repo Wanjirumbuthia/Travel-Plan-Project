fa3c8f999f14d5bc39169d12110814bf2aa284ad
njuguna <rihanna8njuguna@gmail.com>
2024-07-18 15:37:33 +0300

    updated

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -12,9 +12,9 @@ function App() {
     <>
       <Navbar />
       <div>
-        <section><Home /></section>
+        {/* <section><Home /></section>
         <section><Login /></section>
-        <section><PlanNow /></section>
+        <section><PlanNow /></section> */}
         <section><About /></section>
       </div>
     </>
