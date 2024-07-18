82a642147f221899df1bb2ed25d75de18e39b6a1
Gideon Mutai <giddymutai13@gmail.com>
2024-07-18 23:22:56 +0300

    feat: add routing

--- client/src/components/Layout/Landingpage/LandingPage.jsx
+++ client/src/components/Layout/Landingpage/LandingPage.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import "./landingpage.css";
+import { Link } from "react-router-dom";
 // import Home from './Home';
 
 function LandingPage() {
   return (
     <div className="landing-page">
       <header className="header">
-        <div className="logo">TravelExplore</div>
-        <nav className="nav">
-          <ul>
-            {/* <li><a href="#home">Home</a></li> */}
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
+        <div className="logo">Travel Plan</div>
       </header>
       
       <section className="hero">
         <div className="hero-content">
           <h1>Discover Your Next Adventure</h1>
           <p>Explore the world's most beautiful destinations with us.</p>
-          <a href="#gallery" className="cta-button">Explore Now</a>
+          <Link to="/login" className="cta-button">Explore Now</Link>
         </div>
       </section>
       
