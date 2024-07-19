8846f758ded4c333f2d1172ba0c8dfba0e64d5ea
Gideon Mutai <giddymutai13@gmail.com>
2024-07-19 11:52:23 +0300

    feat: add trip card routing

--- client/src/components/Section/Home/Home.jsx
+++ client/src/components/Section/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from "react-router-dom";
 import "./home.css";
 
 function Home() {
@@ -6,32 +7,42 @@ function Home() {
     {
       image: 'https://images.pexels.com/photos/460740/pexels-photo-460740.jpeg?auto=compress&cs=tinysrgb&w=600',
       location: 'France',
-      title: 'Paris'
+      title: 'Paris',
+      btn_txt: 'View Paris',
+      route: '/paris'
     },
     {
       image: 'https://images.pexels.com/photos/13201175/pexels-photo-13201175.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
       location: 'New York',
       title: 'USA',
+      btn_txt: 'View New York',
+      route: '/new_york'
     },
     {
       image: 'https://images.pexels.com/photos/2482308/pexels-photo-2482308.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load',
       location: 'Pyramids of Gaza',
       title: 'Egypt',
+      btn_txt: 'View Egypt'
     },
     {
       image: 'https://images.pexels.com/photos/3123690/pexels-photo-3123690.jpeg?auto=compress&cs=tinysrgb&w=600',
       location: 'Sahara Desert',
       title: 'Morocco',
+      btn_txt: 'View Morocco'
     },
     {
       image: 'https://images.pexels.com/photos/1536436/pexels-photo-1536436.jpeg?auto=compress&cs=tinysrgb&w=600',
       location: 'Opera House',
       title: 'Sydney',
+      btn_txt: 'View Sydney',
+      route: '/sydney'
     },
     {
       image: 'https://images.pexels.com/photos/6580703/pexels-photo-6580703.jpeg?auto=compress&cs=tinysrgb&w=600',
       location: 'Rio de Janeiro',
       title: 'Brazil',
+      btn_txt: 'View Brazil',
+      route: '/brazil'
     }
   ];
 
@@ -62,7 +73,7 @@ function Home() {
           <div className="text">
             <p className="location">{photo.location}</p>
             <p className="title">{photo.title}</p>
-            <button className="link">VIEW TRIP</button>
+            <button className="link"><Link to={photo.route}>{photo.btn_txt}</Link></button>
           </div>
         </div>
       ))}
